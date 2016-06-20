f0fb6ac3a7a4c6208aa863b84c60c8fe20719e5f
Praveen <pkrprawin@gmail.com>
2016-06-20 19:24:46 +0530

    added default string

--- db/migrate/20160617122051_create_histories.rb
+++ db/migrate/20160617122051_create_histories.rb
@@ -2,9 +2,9 @@ class CreateHistories < ActiveRecord::Migration
   def change
     create_table :histories do |t|
       t.references :historiable, polymorphic: true, index: true
-      t.string :title
+      t.string :title, default: ""
       t.text :description
-      t.string :tags
+      t.string :tags, default: ""
 
       t.timestamps null: false
     end
