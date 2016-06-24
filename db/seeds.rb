250303fc62b02b0bace6146dbef0c1f06e4bec7b
Praveen <pkrprawin@gmail.com>
2016-06-24 20:54:59 +0530

    created profile state country model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+countries = [{ name:'India', phone_country_code: '+91', currency: 'INR'}]
+states = ["Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Lakshadweep", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Orissa", "Pondicherry", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh","Uttarakhand", "Uttaranchal", "West Bengal"]
+
+countries.each do |c|
+  country = Country.create!(c)
+  states.each do |s|
+    state = country.states.new(name: s).save!
+  end
+end
+
